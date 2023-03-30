4c7433c6494195f45bccb893e6990fd3e15454e5
CSXRP <CSXRP@LAPTOP-ITVH3M10>
2023-03-30 19:18:26 -0400

    More omework on the sum of prime numbers

---
+++ SinglyLinkedListDemoApp/src/main/java/richard/singlylinkedlistdemoapp/LinkedList.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package richard.singlylinkedlistdemoapp;
+
+/**
+ *
+ * @author CSXRP
+ */
+
+
+class Node {
+   public int data;
+   public Node next;
+
+   public Node(int data) {
+      this.data = data;
+      this.next = null;
+   }
+}
+
+class LinkedList {
+   private Node head;
+   private Node tail;
+    
+   public LinkedList() {
+      head = null;
+      tail = null;
+   }
+    
+   public void append(Node newNode) {
+      if (head == null) {
+         head = newNode;
+         tail = newNode;
+      }
+      else {
+         tail.next = newNode;
+         tail = newNode;
+      }
+   }
+   
+   public void prepend(Node newNode) {
+      if (head == null) {
+         head = newNode;
+         tail = newNode;
+      }
+      else {
+         newNode.next = head;
+         head = newNode;
+      }
+   }
+   
+
+   
+   public void insertAfter(Node currentNode, Node newNode) {
+      if (head == null) {
+         head = newNode;
+         tail = newNode;
+      }
+      else if (currentNode == tail) {
+         tail.next = newNode;
+         tail = newNode;
+      }
+      else {
+         newNode.next = currentNode.next;
+         currentNode.next = newNode;
+      }
+   }
+   
+
+   public void removeAfter(Node currentNode) {
+      if (currentNode == null && head != null) {
+         // Special case: remove head
+         Node succeedingNode = head.next;
+         head = succeedingNode;
+         if (succeedingNode == null) {
+             // Last item was removed
+             tail = null;
+         }
+      }
+      else if (currentNode.next != null) {
+         Node succeedingNode = currentNode.next.next;
+         currentNode.next = succeedingNode;
+         if (succeedingNode == null) {
+            // Remove tail
+            tail = currentNode;
+         }
+      }
+   }
+   public Node getNode(int index){
+        Node currentNode = head;
+        int currentIndex = 0;
+      while (currentNode != null && currentIndex < index){
+        
+          currentNode = currentNode.next;
+          currentIndex++;
+      }
+      return currentNode;
+   }
+   public void printList(){
+      Node currentNode = head;
+      while (currentNode != null){
+          System.out.print(currentNode.data + " " );
+          currentNode = currentNode.next;
+      }
+      System.out.println();
+   }
+}
